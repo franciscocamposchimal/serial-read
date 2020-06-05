0690e8384c3b6bfc82863925d2d890a2c1d215f6
Francisco Campos <camposfrancisci@gmail.com>
2020-06-05 15:30:19 -0500

    feat(data): send fake

--- socket-client.js
+++ socket-client.js
@@ -1,10 +1,36 @@
-var socket = require('socket.io-client')('http://localhost:3000');
-socket.on('connect', () => {
-    console.log('connected');
-    socket.emit('SENSORS_CONNECTION', true);
+const socket = require('socket.io-client')('http://localhost:3000');
+const moment = require('moment');
 
-    socket.emit('SERVER_SOCK', {data: "Hello"});
+function pRandVal() {
+	return Math.floor(Math.random() * (500 - 1 + 1) + 1);
+}
+
+function tRandVal() {
+	return Math.floor(Math.random() * (100 - 1 + 1) + 1);
+}
+
+socket.on('connect', () => {
+	console.log('connected');
+	socket.emit('SENSORS_CONNECTION', true);
+
+	setInterval(() => {
+		const data = {
+			date: moment().format('dddd, MMMM Do YYYY, h:mm:ss a'),
+			sensorsP: [
+				{ name: 's5', type: 'p', val: pRandVal() },
+				{ name: 's6', type: 'p', val: pRandVal() },
+				{ name: 's7', type: 'p', val: pRandVal() }
+			],
+			sensorsT: [
+				{ name: 's1', type: 't', val: tRandVal() },
+				{ name: 's2', type: 't', val: tRandVal() },
+				{ name: 's3', type: 't', val: tRandVal() },
+				{ name: 's4', type: 't', val: tRandVal() }
+			]
+		};
+		socket.emit('SERVER_SOCK', data);
+	}, 1000);
 });
 socket.on('disconnect', () => {
-    console.log('disconnected');
- });
+	console.log('disconnected');
+});
