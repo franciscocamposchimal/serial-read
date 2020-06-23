e4b4698160f67e25a3fc3cca032292abc525a15c
Francisco Campos <camposfrancisci@gmail.com>
2020-06-22 20:43:24 -0500

    feat(https): data format

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function logData(data, socket) {
 	if (bufferData.includes('\n')) {
 		bufferData.split('\n');
 		try {
+			console.log(bufferData);
 			bufferData = bufferData.split('|');
 			if (bufferData.length > 6) {
 				bufferData = bufferData.slice(0,6);
